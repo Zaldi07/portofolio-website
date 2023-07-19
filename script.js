6f66e8089beae52c2816adf225f062470121154b
zaldinugraha07 <zaldinugraha2020@gmail.com>
2023-07-19 12:12:14 +0700

    porto update

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//Aos
 AOS.init({
-  duration: 1200,
+  duration: 3000,
+});
+// swipper
+var swiper = new Swiper('.portofolio-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
